2da46d14d5976651371b95a086c043c7e2fe8e90
Tahar-BRAHAM <brahamtahar97@gmail.com>
2022-02-18 15:43:06 +0100

    completing final touchs, design and add sup features

--- src/app/add-type/add-type.component.ts
+++ src/app/add-type/add-type.component.ts
@@ -1,15 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-
+import axios from 'axios';
+import { Injectable } from '@angular/core';
+@Injectable({ providedIn: 'root' })
 @Component({
   selector: 'app-add-type',
   templateUrl: './add-type.component.html',
   styleUrls: ['./add-type.component.css']
 })
 export class AddTypeComponent implements OnInit {
-
+  title: any
+  description: any
+  datatype:any
+  user:any
+  id_type: any
+  label_type:any
+  image_type : any
   constructor() { }
-
-  ngOnInit(): void {
+  add() {
+    var option = {
+      label_type: this.label_type,
+      image_type: this.image_type,
+    };
+    console.log(option);
+    axios
+      .post("http://localhost:3001/api/items/type", option)
+      .then((response) => {
+        console.log(response);
+        location.reload();
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
-
+  ngOnInit(): void {
+    axios.get("http://localhost:3001/api/items/type").then((response) => {
+      this.datatype = response.data;
+      console.log(this.datatype);
+      // localStorage.setItem("type", JSON.stringify(this.type));
+    });
+    this.user= JSON.parse(localStorage.getItem("user")!)
+  }
 }
--- src/app/post-lecture/post-lecture.component.ts
+++ src/app/post-lecture/post-lecture.component.ts
@@ -13,19 +13,45 @@ import { Router } from '@angular/router';
 export class PostLectureComponent implements OnInit {
 
   constructor(private router: Router) { }
+  user:any;
   title: string = '';
   description: string = '';
   image_post: string = '';
   type:any;
   container:any=[];
+  id_type:any;
+  array:any=[];
 
   async getPostFormData(data: any) {
     data.preventDefault();
+    if(this.id_type === "web development"){
+      this.id_type = 1
+    }if(this.id_type === "Math"){
+      this.id_type = 2
+    }if(this.id_type === "Science"){
+      this.id_type = 3
+    }if(this.id_type === "English"){
+      this.id_type = 4
+    }if(this.id_type === "Mobile development"){
+      this.id_type = 5
+    }if(this.id_type === "French"){
+      this.id_type = 6
+    }if(this.id_type === "Arabic"){
+      this.id_type = 7
+    }if(this.id_type === "Physics"){
+      this.id_type = 8
+    }if(this.id_type === "Technical science"){
+      this.id_type = 9
+    }if(this.id_type === "Philosophy"){
+      this.id_type = 10
+    }
     await axios
       .post('http://localhost:3001/api/items/posts', {
+        id_type:this.id_type,
         title: this.title,
         description: this.description,
-        image_post: this.image_post
+        image_post: this.image_post,
+        id_user:this.user.id_user
       }).then((response) => {
         if (response.data) {
           localStorage.setItem(
@@ -36,7 +62,7 @@ export class PostLectureComponent implements OnInit {
               image_post: this.image_post
             })
           )
-          this.router.navigate(['/feed'])
+          this.router.navigate(['/'])
         }
       })
   }
@@ -49,7 +75,7 @@ export class PostLectureComponent implements OnInit {
     formData.append("upload_preset", "ehzqyvxt")
     formData.append("cloud_name", "brahamtahar")
     axios
-      .post("http://api.cloudinary.com/v1_1/campgo/upload", formData)
+      .post("http://api.cloudinary.com/v1_1/brahamtahar/upload", formData)
       .then((result) => {
         this.image_post = result.data.url
       })
@@ -60,12 +86,12 @@ export class PostLectureComponent implements OnInit {
     axios
     .get('http://localhost:3001/api/items/type')
     .then((result)=>{
-      (result.data.forEach((element:any) => {
-        this.container.push(element.label_type)  //map to render the types, but error mySql because no id_type !
-      }))
+      this.array=result.data
+      console.log(this.array, "array")
       this.type=this.container
       console.log(this.type,"container")
     })
+    this.user = JSON.parse(localStorage.getItem("user")!)
   }
 }
 
